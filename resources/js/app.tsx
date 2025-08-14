3538623d771e3a0f88182abfff0ed43df050ba5f
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-08-14 14:16:58 +0800

    updated tests.yml

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,15 +4,11 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
-import { scan } from "react-scan"; // must be imported before React and React DOM
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-scan({
-  enabled: true,
-});
 createInertiaApp({
-    title: (title) => title ? `${title} - ${appName}` : appName,
+    title: (title) => (title ? `${title} - ${appName}` : appName),
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
