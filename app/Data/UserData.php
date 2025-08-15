71fdecc453d5a304492ee645810ab6567d1f538b
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-08-15 08:26:19 +0800

    react scan added

--- app/Data/UserData.php
+++ app/Data/UserData.php
@@ -3,12 +3,16 @@
 namespace App\Data;
 
 use Carbon\CarbonInterface;
+use Spatie\LaravelData\Attributes\Validation\Email;
+use Spatie\LaravelData\Attributes\Validation\Max;
 use Spatie\LaravelData\Data;
 
 class UserData extends Data
 {
     public function __construct(
+        #[Max(255)]
         public readonly string $name,
+        #[Max(255),Email]
         public readonly string $email,
         public readonly ?string $password,
         public readonly ?CarbonInterface $email_verified_at,
