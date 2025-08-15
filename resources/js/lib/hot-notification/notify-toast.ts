a0be2a07dcd934fc20280ab23ff4b5560bdc8aa1
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-08-16 07:12:54 +0800

    added custom react-hot-toaster

---
+++ resources/js/lib/hot-notification/notify-toast.ts
@@ -0,0 +1,5 @@
+import toast from "react-hot-toast";
+
+ export const notifyToast = toast;
+
+ // the Toaster component is in AppSidebarLayout and the examples is in the dashboard page
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,6 +1,10 @@
+import { Button } from '@/components/ui/button';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
+import { playAudio } from '@/lib/audios/play-audio-message';
+import { notifyToast } from '@/lib/hot-notification/notify-toast';
 import { type BreadcrumbItem } from '@/types';
+
 import { Head } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -9,6 +13,7 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/dashboard',
     },
 ];
+
 
 export default function Dashboard() {
     return (
@@ -27,7 +32,39 @@ export default function Dashboard() {
                     </div>
                 </div>
                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className='flex justify-center p-5 flex-wrap  gap-5'>
+                        <Button onClick={() => {
+                            notifyToast.success('Success Toast')
+                            playAudio('success')
+
+                        }}>
+                            Success Toast with success sound
+                        </Button>
+                        <Button onClick={() => {
+                            notifyToast.error('Error Toast')
+                            playAudio('error')
+                        }}>
+                            Error Toast with error sound
+                        </Button>
+                        <Button onClick={() => notifyToast.loading('Loading Toast')}>
+                            Loading Toast
+                        </Button>
+                        <Button onClick={() => notifyToast.dismiss()}>
+                            Remove All Toast
+                        </Button>
+                        <Button onClick={() => {
+                            notifyToast('Info Toast')
+                            playAudio('info')
+                        }}>
+                            Info Toast with info sound
+                        </Button>
+                        <Button onClick={() => {
+                            notifyToast('Message received toast')
+                            playAudio('message')
+                        }}>
+                            Message received toast
+                        </Button>
+                    </div>
                 </div>
             </div>
         </AppLayout>
