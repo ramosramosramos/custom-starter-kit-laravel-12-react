b7be98eab9afd6c1d55607ce89faf4563a2e8fc6
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-08-26 23:19:05 +0800

    Format actions

--- app/Actions/User/UpdateUserPasswordAction.php
+++ app/Actions/User/UpdateUserPasswordAction.php
@@ -12,6 +12,13 @@ class UpdateUserPasswordAction
 {
     use AsAction;
 
+    /**
+     * Summary of handle
+     *
+     * @param  \App\Models\User  $user
+     * @param  string  $password
+     * @return void
+     */
     public function handle(User $user, string $password): void
     {
         $user->update([
